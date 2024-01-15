12e7d8bce6d606f9b728908856f854cf7ae40999
santhusri <santhoshkumar.ramakoti@gmail.com>
2024-01-15 07:47:05 +0530

    my task has been done

--- myJavafile.java
+++ myJavafile.java
@@ -1,2 +1,12 @@
+<<<<<<< Updated upstream
 this is my java file
-this my manager work completed
+his my manager work completed
+=======
+this myjava file 
+1st day java code comleted
+2nd day java code comleted
+
+3rd day java code comleted
+4th day java code comleted
+5th day of code completed 
+>>>>>>> Stashed changes
